792f2ede7e445114387955aac80086a7ab45af20
sivanel <sivanelkabes@gmail.com>
2019-07-07 21:52:55 +0300

    created 3 files: 1. GetRouteById 2. PostFullScreen 3. layout_post_full_screen

---
+++ app/src/main/java/com/TripShare/Client/CommunicationWithServer/GetRouteByID.java
@@ -0,0 +1,65 @@
+package com.TripShare.Client.CommunicationWithServer;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.ResponseHandler;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.client.utils.URIBuilder;
+import cz.msebera.android.httpclient.impl.client.BasicResponseHandler;
+import cz.msebera.android.httpclient.impl.client.HttpClientBuilder;
+
+public class GetRouteByID extends AsyncTask<String, Integer, String>
+{
+    String body;
+    SetRetrievedRoute m_listenerAddItems;
+    long m_routeID;
+
+    public GetRouteByID(SetRetrievedRoute i_listener, long i_routeID)
+    {
+        m_listenerAddItems = i_listener;
+        m_routeID = i_routeID;
+    }
+    @Override
+    protected String doInBackground(String... Args)
+    {
+        String output = null;
+
+        try
+        {
+            HttpClient httpClient = HttpClientBuilder.create().build();
+
+            // Build URI
+            URIBuilder builder = new URIBuilder("http://10.0.2.2:8080/SaveRouteToDB/RouteByIDServlet");
+            // TODO: change "0" to m_userID
+            builder.setParameter("m_routeID", String.valueOf(m_routeID));
+
+            // Send request to server
+            HttpGet http_get = new HttpGet(builder.build());
+            HttpResponse response = httpClient.execute(http_get);
+
+            // Handle response
+            ResponseHandler<String> handler = new BasicResponseHandler();
+            body = handler.handleResponse(response);
+            int code = response.getStatusLine().getStatusCode();
+
+            // retrieve the list of routes from response need to update the list view in the main thread.
+            if(code == 200)
+            {
+                m_listenerAddItems.setRetrievedRoute(body);
+            }
+        }
+        catch (Exception e)
+        {
+            Log.e("log_tag", "Error in http connection " + e.toString());
+            output = "Error in http connection " + e.toString();
+        }
+        return output;
+    }
+
+    public interface SetRetrievedRoute
+    {
+        void setRetrievedRoute(String i_body);
+    }
+}
