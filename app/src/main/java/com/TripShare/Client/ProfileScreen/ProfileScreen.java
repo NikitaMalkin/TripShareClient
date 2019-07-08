64ef2692c2c1ee16986b76b00a52c5515dc681cb
sivanel <sivanelkabes@gmail.com>
2019-07-08 11:30:48 +0300

    master2 to check if works correctly for Nick

--- app/src/main/java/com/TripShare/Client/ProfileScreen/ProfileScreen.java
+++ app/src/main/java/com/TripShare/Client/ProfileScreen/ProfileScreen.java
@@ -3,35 +3,44 @@ package com.TripShare.Client.ProfileScreen;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import com.TripShare.Client.Common.ActivityWithNavigationDrawer;
 import android.widget.Toast;
+import com.TripShare.Client.Common.DrawerAdapter;
 import com.TripShare.Client.Common.Post;
 import com.TripShare.Client.CommunicationWithServer.GetPostsFromDB;
 import com.TripShare.Client.PostFullScreen.PostFullScreen;
 import com.TripShare.Client.R;
-import com.TripShare.Client.Common.Post;
 import com.google.gson.Gson;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class ProfileScreen extends AppCompatActivity implements GetPostsFromDB.AddAllItemsToListViewListener, PostsAdapter.shareButtonClickedListener, PostsAdapter.mapButtonClickedListener
+public class ProfileScreen extends ActivityWithNavigationDrawer implements GetPostsFromDB.AddAllItemsToListViewListener, PostsAdapter.shareButtonClickedListener, PostsAdapter.mapButtonClickedListener
 {
-    ArrayList<PostItem> m_posts;
-    PostsAdapter m_adapter;
+    private ArrayList<PostItem> m_posts;
+    private PostsAdapter m_PostAdapter;
+    private DrawerAdapter m_drawerAdapter;
     int m_firstPositionToRetrieve;
+    private RecyclerView m_drawerRecyclerView;
     Gson gson = new Gson();
+    private RecyclerView.LayoutManager m_layoutManager;
+    private ActionBarDrawerToggle m_DrawerToggle;
+    private DrawerLayout m_DrawerLayout;
+    private String m_ActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_screen);
+        setActivityTitle("Profile");
 
         RecyclerView Posts = findViewById(R.id.profileScreen_recyclerView);
 
@@ -41,7 +50,7 @@ public class ProfileScreen extends AppCompatActivity implements GetPostsFromDB.A
         PostsAdapter adapter = new PostsAdapter(m_posts, this, this);
         // Attach the adapter to the recyclerview to populate items
         Posts.setAdapter(adapter);
-        m_adapter = adapter;
+        m_PostAdapter = adapter;
 
         // Initialize contacts
         // m_posts = PostItem.getPosts(); //TODO this method is responsible for gathering the first couple posts
@@ -57,9 +66,13 @@ public class ProfileScreen extends AppCompatActivity implements GetPostsFromDB.A
         // Set layout manager to position the items
         Posts.setLayoutManager(new LinearLayoutManager(this));
 
+        //Posts RecyclerView separator initialization
         DividerItemDecoration decoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
         decoration.setDrawable(getDrawable(R.drawable.shadow));
         Posts.addItemDecoration(decoration);
+
+        //Application Drawer initialization
+        initializeDrawerLayout();
     }
 
     public void imageButton_refreshPostsOnClick(View view)
@@ -110,7 +123,7 @@ public class ProfileScreen extends AppCompatActivity implements GetPostsFromDB.A
         // TODO: change the default picture to an acutal picture from the route if exists
         PostItem itemToAdd = new PostItem(i_postToAdd, ContextCompat.getDrawable(getApplicationContext(), R.drawable.post_thumbnail_sample));
         m_posts.add(itemToAdd);
-        m_adapter.notifyDataSetChanged();
+        m_PostAdapter.notifyDataSetChanged();
     }
 
     @Override
